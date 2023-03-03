8dfdcde24c38f954eff80b29129e95ed2040dd97
Katarzyna Śliwka <katarzynasliwka@Katarzynas-MacBook-Pro.local>
2023-03-03 13:38:57 +0100

    gradient grainy circle

---
+++ src/components/Circle.js
@@ -0,0 +1,7 @@
+import classes from './Circle.module.css'
+
+function Circle(){
+    return <div className={classes.circle}></div>
+}
+
+export default Circle
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,19 @@
 import classes from "./Home.module.css";
+import Circle from "./Circle";
 
 function HomePage() {
   return (
     <>
       <div className={classes.container}>
         <h1>
-          Hi! My name is Katarzyna and I am Frontend developer, living in
-          Poland.
+          Hi! My name is Katarzyna and I am Frontend Developer.
         </h1>
         <h3>PORTFOLIO BY KATARZYNA ŚLIWKA</h3>
+        <Circle className={classes.circle}/>
         <a className={classes.scroll_down}>Scroll Down</a>
+        
       </div>
+      
     </>
   );
 }
