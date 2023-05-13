f4ee47d798ae4515103f7642d4c3d93c737806d0
Katarzyna Śliwka <katarzynasliwka@Katarzynas-MacBook-Pro.local>
2023-05-13 10:08:55 +0200

    utils cleanup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,25 @@
-import { Fragment, useEffect, useState, useRef } from "react";
-import Spinner from 'react-bootstrap/Spinner';
+import {  useRef } from "react";
 
 import Tools from "./components/Tools";
 import NavBar from "./components/NavBar";
 import HomePage from "./components/Home";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
-import Footer from "./components/Footer"
-// import useLocoScroll from "./components/Hooks/useLocoScroll";
-import LocomotiveScroll from 'locomotive-scroll';
+import Footer from "./components/Footer";
 function App() {
   const ref = useRef(null);
 
-  // useLocoScroll()
-
-
-
-  // if (typeof window === "undefined" || !window.document) {
-  //   return null;
-  // }
-  // const options = {
-  //   smooth: true,
-  // } 
-
   let container = useRef(null);
-    
-//   useEffect(() => {
-     
-//      new LocomotiveScroll({
-//         el: container,
-//         smooth: true,
-//         lerp: .06,
-//         multiplier: .5
-//      });
-
-//   }, []);
 
   return (
-
-      // <LocomotiveScrollProvider options={options} containerRef={ref}>
-      <div  data-scroll-container  ref={el => container = el} >
+    <div data-scroll-container ref={(el) => (container = el)}>
       <NavBar />
       <HomePage />
       <Tools />
       <Projects />
       <Contact />
-      <Footer/>
-      </div>
-      // </LocomotiveScrollProvider>
-
-      
-   
+      <Footer />
+    </div>
   );
 }
 
